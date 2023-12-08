53c13984ca45292a77c30089746d353630cec6d4
Paulina <paulinasfielek@gmail.com>
2023-12-08 13:17:09 -0800

    added featured puppy & styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,28 @@ import {puppyList} from './data.js'
 console.log(puppyList);
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId);
+  
   console.log(puppies);
 
   return (
-    <div>
-      {puppies.map((puppy) => {
-        return <p key={puppy.id}> {puppy.name} </p>;
-      })}
-    </div>
+    <section id="maindiv">
+      <div id="puppydiv">
+        {puppies.map((puppy) => {
+          return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id} className="puppy"> {puppy.name} </p>;
+        })}
+        </div>
+        {featPupId && (
+        <div className="featuredPup">
+          <h2> {featuredPup.name} </h2>
+          <ul>
+            <li> Age: {featuredPup.age} </li>
+            <li> Email: {featuredPup.email} </li>
+          </ul>
+        </div>
+      )}
+    </section>
   )
 }
 
